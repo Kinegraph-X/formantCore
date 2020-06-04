c9f28ac9d6e67da5e7c583cda182dc2a7790ae52
vAsSiLyB <vassilyb.r@gmail.com>
2020-06-05 00:31:43 +0200

    Added a bunch of (already used by other projects) functions to the string.js toolbox

--- src/utilities/string.js
+++ src/utilities/string.js
@@ -14,3 +14,187 @@ Object.defineProperty(String.prototype, 'splice', {
 		return this.slice(0, index) + (add || '') + this.slice(index + count);
 	}
 });
+
+function padNumber(n, width, z) {
+  z = String(z) || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+//https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+	Object.defineProperty(String.prototype, 'padStart', {
+			value : function padStart(targetLength,padString) {
+					     targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+					     padString = String(padString || ' ');
+					     if (this.length > targetLength) {
+					         return String(this);
+					     }
+					     else {
+					         targetLength = targetLength-this.length;
+					         if (targetLength > padString.length) {
+					             padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+					         }
+					         return padString.slice(0,targetLength) + String(this);
+					     }
+					 },
+					enumerable : false,
+					writable : false,
+					configurable : false
+				}
+	);
+}
+
+Object.defineProperty(String.prototype, 'dromedarToHyphens', { 						// accepts camel & dromedar
+	value : function() {
+				return this.replace(/[A-Z]/g, function(match, offset, str) {
+					return (offset > 0 ? '-' : '') + String(match).toLowerCase() 	// accept camel & dromedar
+				});
+			},
+			enumerable : false,
+			writable : false,
+			configurable : false
+	}
+);
+
+Object.defineProperty(String.prototype, 'toFirstLetterUpperCase', {
+	value : function() {
+				return this.replace(/^[a-z]/, function(match, offset, str) {
+					return String(match).toUpperCase();
+				});
+			},
+			enumerable : false,
+			writable : false,
+			configurable : false
+	}
+);
+
+Object.defineProperty(String.prototype, 'hexEncode', {
+	value : function(){
+				var hex, i;
+				
+				var result = "";
+				for (i = 0; i < this.length; i++) {
+				  hex = this.charCodeAt(i).toString(16);
+				  result += ("000"+hex).slice(-4) + ' ';
+				}
+				
+				return result
+			},
+			enumerable : false,
+			writable : false,
+			configurable : false
+			}
+);
+
+Object.defineProperty(String.prototype, 'indentOnXMLTags', {
+	value : function(){
+				var lineCounter = 0; depthCounter = -1, returnedString = '', d = 0, indentChars = '';
+				for(var i = 0, l = this.length; i < l; i++) {
+					d = 0, indentChars = '';
+					if (this.charAt(i) === '<') {
+						if (this.charAt(i + 1) !== '/')
+							depthCounter++;
+						
+						indentChars += '\n';
+						for(d; d < depthCounter; d++) {
+							indentChars += '\t';
+						}
+						returnedString += indentChars + this.charAt(i);
+						
+						if (this.charAt(i + 1) === '/')
+							depthCounter--;
+					}
+					else if (this.charAt(i) === '>') {
+						if (this.charAt(i + 1) !== '<')
+							indentChars += '\n';
+						for(d; d < depthCounter + 1; d++) {
+							indentChars += '\t';
+						}
+						returnedString += this.charAt(i) + indentChars;
+					}
+					else {
+						returnedString += this.charAt(i)
+					}
+				}
+				return returnedString;
+			},
+			enumerable : false,
+			writable : false,
+			configurable : false
+			}
+);
+
+Object.defineProperty(String.prototype, 'nbsp2ws', {
+	value : function(){
+		return this.replace(/&nbsp;/g, ' ');
+	},
+	enumerable : false,
+	writable : false,
+	configurable : false
+	}
+);
+Object.defineProperty(String.prototype, 'ws2nbsp', {
+	value : function(){
+		return this.replace(/ /g, '&nbsp;');
+	},
+	enumerable : false,
+	writable : false,
+	configurable : false
+	}
+);
+
+/**
+ * Method sortByPropName
+ * 
+ * returns Array(obj[propNameA], obj[propNameB], obj[propNameC], etc.)
+ */
+Object.defineProperty(Object.prototype, 'sortByPropName', {
+	writable : false,
+	value : function () {
+		var keys = Object.keys(this).sort();
+//		console.log(keys.join(''))
+		var arr = []; 
+		for(var i = 0, l = keys.length; i < l; i++) {
+			arr.push(this[keys[i]]);
+		}
+		return arr;
+	}
+});
+
+RegExp.escapeLitteral = /[-\/\\^$*+?.()|[\]{}]/g;
+
+RegExp.escape= function(s) {
+    return s.replace(this.escapeLitteral, '\\$&');
+};
+RegExp.protect= function(s) {
+	return s.replace(/\\/g, '\\\\');
+};
+
+// Source: http://www.antiyes.com/jquery-blink-plugin
+//http://www.antiyes.com/jquery-blink/jquery-blink.js
+(function($) {
+ $.fn.blink = function(options) {
+     var defaults = {
+         delay: 113,
+         iterations : 3
+     };
+     var options = $.extend(defaults, options);
+
+     return this.each(function() {
+         var obj = $(this), inter, iterations = 0;
+         inter = setInterval(function() {
+             if ($(obj).css("visibility") == "visible") {
+                 $(obj).css('visibility', 'hidden');
+             }
+             else {
+                 $(obj).css('visibility', 'visible');
+             }
+             iterations++;
+             if (iterations / 2 === options.iterations)
+            	 clearInterval(inter);
+         }, options.delay);
+     });
+ }
+}(jQuery)) 
