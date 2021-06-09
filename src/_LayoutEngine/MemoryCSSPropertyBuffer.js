7263bcf3035627d0b1dc01d0720bc92f88c9fe5d
sylvain <vassilyB@gmail.com>
2021-06-09 16:06:32 +0200

    Intermediate Commit: DataStructures for CSSPropertyBuffer definition

--- src/_LayoutEngine/MemoryCSSPropertyBuffer.js
+++ src/_LayoutEngine/MemoryCSSPropertyBuffer.js
@@ -19,6 +19,11 @@ var MemoryCSSPropertyBuffer = function(initialLoad) {
 MemoryCSSPropertyBuffer.prototype = Object.create(Uint8Array.prototype);
 MemoryCSSPropertyBuffer.prototype.objectType = 'MemoryCSSPropertyBuffer';
 
+
+
+
+
+
 //var sample = {
 //	token: "DIMENSION",
 //	value: 1,
@@ -27,52 +32,25 @@ MemoryCSSPropertyBuffer.prototype.objectType = 'MemoryCSSPropertyBuffer';
 //	unit: "px"
 //}
 
-//\w+\.prototype\.tokenType\s?=\s?"[^"]+";
-
-//BadStringToken.prototype.tokenType = "BADSTRING";
-//BadURLToken.prototype.tokenType = "BADURL";
-//WhitespaceToken.prototype.tokenType = "WHITESPACE";
-//CDOToken.prototype.tokenType = "CDO";
-//CDCToken.prototype.tokenType = "CDC";
-//ColonToken.prototype.tokenType = ":";
-//SemicolonToken.prototype.tokenType = ";";
-//CommaToken.prototype.tokenType = ",";
-//OpenCurlyToken.prototype.tokenType = "{";
-//CloseCurlyToken.prototype.tokenType = "}";
-//OpenSquareToken.prototype.tokenType = "[";
-//CloseSquareToken.prototype.tokenType = "]";
-//OpenParenToken.prototype.tokenType = "(";
-//CloseParenToken.prototype.tokenType = ")";
-//IncludeMatchToken.prototype.tokenType = "~=";
-//DashMatchToken.prototype.tokenType = "|=";
-//PrefixMatchToken.prototype.tokenType = "^=";
-//SuffixMatchToken.prototype.tokenType = "$=";
-//SubstringMatchToken.prototype.tokenType = "*=";
-//ColumnToken.prototype.tokenType = "||";
-//EOFToken.prototype.tokenType = "EOF";
-//DelimToken.prototype.tokenType = "DELIM";
-//IdentToken.prototype.tokenType = "IDENT";
-//FunctionToken.prototype.tokenType = "FUNCTION";
-//AtKeywordToken.prototype.tokenType = "AT-KEYWORD";
-//HashToken.prototype.tokenType = "HASH";
-//StringToken.prototype.tokenType = "STRING";
-//URLToken.prototype.tokenType = "URL";
-//NumberToken.prototype.tokenType = "NUMBER";
-//PercentageToken.prototype.tokenType = "PERCENTAGE";
-//DimensionToken.prototype.tokenType = "DIMENSION";
-
-
-
-
 MemoryCSSPropertyBuffer.prototype.optimizedBufferSchema = BinarySchemaFactory(
 	'compactedViewOnProperty',
 	[
-
+		'tokenType',
+		'propertyValue',
+		'propertyType',
+		'repr',
+		'unit'
 	],
 	[
-
+		1,
+		2,
+		1,
+		3,
+		1
 	]
 );
+
+
 
 
 
